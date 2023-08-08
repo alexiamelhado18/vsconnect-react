33ece46e3267255feb9275ab6688c1c249ceca10
Aléxia <aleeevitoria@gmail.com>
2023-08-08 14:52:32 -0300

    inclusão de função mostrar menu

--- vsconnect/src/pages/Home/index.tsx
+++ vsconnect/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+//estilização
 import "./style.css";
 
+//imagens
 import imgLogo from "../../assets/images/logo.svg";
 import imgClientes from "../../assets/images/clientes.png";
 import imgMaosDev from "../../assets/images/maos_dev.png";
@@ -10,28 +12,54 @@ import iconInsta from "../../assets/images/instagram.svg";
 import iconLinkedin from "../../assets/images/linkedin.svg";
 
 function Home() {
+
+
+
+    // Mostrar menu no layout responsivo
+    function mostrarMenu() {
+        let sombra = document.getElementById("sombra") as HTMLCanvasElement; // Obtém o elemento da sombra pelo ID
+        let menu: any = document.getElementById("menu_links"); // Obtém o elemento do menu pelo ID
+        let menu_barras: any = document.getElementById("menu_barras"); // Obtém o elemento do botão do menu pelo ID
+
+        let body: any = document.getElementsByTagName("body")[0]; // Obtém o elemento body (primeiro elemento)
+
+        if (window.getComputedStyle(menu).left != "10px") { // Verifica se a propriedade 'left' do estilo computado do elemento do menu é diferente de 10px
+            menu.style.left = "10px"; // Define a posição 'left' do menu como 10px movendo ele para direita
+            sombra.style.right = "-10vw"; // Move a sombra para a direita
+            menu_barras.setAttribute("aria-expanded", "true"); // Atualiza o atributo 'aria-expanded' para 'true'
+            menu_barras.setAttribute("aria-label", "fechar menu"); // Atualiza o atributo 'aria-label' para 'fechar menu'
+            body.style.overflow = "hidden"; // Define o overflow do body como "hidden" para evitar a rolagem da página
+        } else {
+            menu.style.left = "-300px"; // Esconde o menu deslocando-o para esquerda
+            sombra.style.right = "110vw"; // Move a sombra para a esquerda para ocultá-la
+            menu_barras.setAttribute("aria-expanded", "false"); // Atualiza o atributo 'aria-expanded' para 'false'
+            menu_barras.setAttribute("aria-label", "abrir menu"); // Atualiza o atributo 'aria-label' para 'abrir menu'
+            body.style.overflow = "auto"; // Restaura o overflow do body para o valor padrão (auto)
+        }
+        menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
+    }
     return (
         <div>
             <div id="sombra"></div>
             <header>
                 <div className="container header_conteudo">
-                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" onClick={mostrarMenu()} href="#"><div className="barras"></div></a>
+                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" onClick={mostrarMenu} href="#"><div className="barras"></div></a>
                     <img className="header_logo" src={imgLogo} alt="" />
-                        <nav>
-                            <div id="menu_links" className="menu_links">
-                                <a href="../Home/index.html">home</a>
-                                <a href="#">serviços</a>
-                                <a href="../CadastroDev/index.html">cadastro</a>
-                            </div>
-                            <a className="header_icone_login" href="../Login/index.html">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 512 512">
-                                    <path
-                                        d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
-                                </svg>
-                            </a>
-                            <a className="header_botao_login" href="../Login/index.html">login</a>
-                        </nav>
+                    <nav>
+                        <div id="menu_links" className="menu_links">
+                            <a href="../Home/index.html">home</a>
+                            <a href="#">serviços</a>
+                            <a href="../CadastroDev/index.html">cadastro</a>
+                        </div>
+                        <a className="header_icone_login" href="../Login/index.html">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512">
+                                <path
+                                    d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
+                            </svg>
+                        </a>
+                        <a className="header_botao_login" href="../Login/index.html">login</a>
+                    </nav>
                 </div>
             </header>
             <main id="main">
@@ -54,16 +82,16 @@ function Home() {
                     </div>
                     <div className="clientes_conteudo">
                         <img src={imgClientes} alt="" />
-                            <div className="clientes_texto">
-                                <h2>para clientes</h2>
-                                <ul>
-                                    <li>cadastrar serviços</li>
-                                    <li>procurar por desenvolvedores</li>
-                                </ul>
-                                <div>
-                                    <a className="botao clientes_botao_cli" href="#">criar conta</a>
-                                </div>
+                        <div className="clientes_texto">
+                            <h2>para clientes</h2>
+                            <ul>
+                                <li>cadastrar serviços</li>
+                                <li>procurar por desenvolvedores</li>
+                            </ul>
+                            <div>
+                                <a className="botao clientes_botao_cli" href="#">criar conta</a>
                             </div>
+                        </div>
                     </div>
                 </section>
                 <div className="conexao"></div>
@@ -78,24 +106,24 @@ function Home() {
                             <a className="botao clientes_botao_devs" href="#">criar conta</a>
                         </div>
                     </div>
-                    <img src={imgMaosDev} alt=""/>
+                    <img src={imgMaosDev} alt="" />
                 </section>
                 <section className="container servicos">
                     <h2>serviços</h2>
                     <div className="servicos_cards">
                         <div className="card card1">
-                            <img src={imgAlfinete} alt=""/>
-                                <h3>Aplicativo para PetShop</h3>
-                                <p>Possuo um Petshop e gostaria de um aplicativo para que meus clientes pudessem agendar serviços
-                                    como: banho e tosa, taxi dog, hotelzinho e etc.</p>
-                                <p>Proposta: A Combinar</p>
+                            <img src={imgAlfinete} alt="" />
+                            <h3>Aplicativo para PetShop</h3>
+                            <p>Possuo um Petshop e gostaria de um aplicativo para que meus clientes pudessem agendar serviços
+                                como: banho e tosa, taxi dog, hotelzinho e etc.</p>
+                            <p>Proposta: A Combinar</p>
                         </div>
                         <div className="card card2">
-                            <img src={imgAlfinete} alt=""/>
-                                <h3>Site para Pizzaria</h3>
-                                <p>Preciso de um site para divulgar o sabores das pizzas, o endereço, os telefones de contato,
-                                    nossas redes sociais.</p>
-                                <p>Proposta: R$750,00</p>
+                            <img src={imgAlfinete} alt="" />
+                            <h3>Site para Pizzaria</h3>
+                            <p>Preciso de um site para divulgar o sabores das pizzas, o endereço, os telefones de contato,
+                                nossas redes sociais.</p>
+                            <p>Proposta: R$750,00</p>
                         </div>
                     </div>
                     <a href="#">Ver mais serviços</a>
@@ -104,12 +132,12 @@ function Home() {
             <footer>
                 <div className="container rodape">
                     <div className="span_dicas">
-                        <img src={imgDev} alt=""/>
-                            <div className="span_dicas_texto">
-                                <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
-                                    recomendações para saber mais.</p>
-                                <a className="botao botao_dicas" href="#">mais dicas</a>
-                            </div>
+                        <img src={imgDev} alt="" />
+                        <div className="span_dicas_texto">
+                            <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
+                                recomendações para saber mais.</p>
+                            <a className="botao botao_dicas" href="#">mais dicas</a>
+                        </div>
                     </div>
                     <div className="rodape_conteudo">
                         <div className="rodape_conteudo_paginas">
@@ -122,16 +150,16 @@ function Home() {
                                 <li>Cadastrar Desenvolvedor</li>
                             </ul>
                         </div>
-                        <img src={imgLogo} alt=""/>
-                            <div className="rodape_conteudo_contatos">
-                                <h2>Contatos</h2>
-                                <div>
-                                    <a href="#"><img src={iconFace} alt=""/></a>
-                                    <a href="#"><img src={iconInsta} alt=""/></a>
-                                    <a href="#"><img src={iconLinkedin} alt=""/></a>
-                                </div>
-                                <a href="mailto:">contato@vsconnect.com</a>
+                        <img src={imgLogo} alt="" />
+                        <div className="rodape_conteudo_contatos">
+                            <h2>Contatos</h2>
+                            <div>
+                                <a href="#"><img src={iconFace} alt="" /></a>
+                                <a href="#"><img src={iconInsta} alt="" /></a>
+                                <a href="#"><img src={iconLinkedin} alt="" /></a>
                             </div>
+                            <a href="mailto:">contato@vsconnect.com</a>
+                        </div>
                     </div>
                     <p>todos os direitos reservados ©.</p>
                 </div>
