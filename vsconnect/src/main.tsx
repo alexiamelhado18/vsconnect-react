e2a6313ba950c2a2efca31db0a76fe6ccb38dda8
Aléxia <aleeevitoria@gmail.com>
2023-08-08 14:51:17 -0300

    chamada do componente Home e importando css global

--- vsconnect/src/main.tsx
+++ vsconnect/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+//componentes
+import Home from "./pages/Home/";
+
+//estilização global
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
