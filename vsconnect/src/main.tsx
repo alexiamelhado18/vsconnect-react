ceed7174c05758a1dd9e00df3bc209aa9608558f
Aléxia <aleeevitoria@gmail.com>
2023-08-09 22:03:03 -0300

    chamada de footer

--- vsconnect/src/main.tsx
+++ vsconnect/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 //componentes
 import Home from "./pages/Home/";
 import ListaServicos from "./pages/ListaServicos/";
+import Footer from "./components/Footer"
 
 //estilização global
 import "./index.css";
@@ -11,6 +12,8 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Home />
+    <Footer/>
+
     {/* <ListaServicos /> */}
   </React.StrictMode>,
 )
