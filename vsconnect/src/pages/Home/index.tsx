f97f7c66ad3fb4912d81248ac76680830838f3fc
Aléxia <aleeevitoria@gmail.com>
2023-08-09 22:02:52 -0300

    remoção do footer e mudança de id da main da home

--- vsconnect/src/pages/Home/index.tsx
+++ vsconnect/src/pages/Home/index.tsx
@@ -6,14 +6,9 @@ import imgLogo from "../../assets/images/logo.svg";
 import imgClientes from "../../assets/images/clientes.png";
 import imgMaosDev from "../../assets/images/maos_dev.png";
 import imgAlfinete from "../../assets/images/alfinete.png";
-import imgDev from "../../assets/images/dev.png";
-import iconFace from "../../assets/images/facebook.svg";
-import iconInsta from "../../assets/images/instagram.svg";
-import iconLinkedin from "../../assets/images/linkedin.svg";
+
 
 function Home() {
-
-
 
     // Mostrar menu no layout responsivo
     function mostrarMenu() {
@@ -38,6 +33,7 @@ function Home() {
         }
         menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
     }
+
     return (
         <div>
             <div id="sombra"></div>
@@ -62,7 +58,7 @@ function Home() {
                     </nav>
                 </div>
             </header>
-            <main id="main">
+            <main id="main_home">
                 <h1>página inicial VSconnect</h1>
                 <section className="banner">
                     <div className="banner_conteudo">
@@ -129,41 +125,6 @@ function Home() {
                     <a href="#">Ver mais serviços</a>
                 </section>
             </main>
-            <footer>
-                <div className="container rodape">
-                    <div className="span_dicas">
-                        <img src={imgDev} alt="" />
-                        <div className="span_dicas_texto">
-                            <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
-                                recomendações para saber mais.</p>
-                            <a className="botao botao_dicas" href="#">mais dicas</a>
-                        </div>
-                    </div>
-                    <div className="rodape_conteudo">
-                        <div className="rodape_conteudo_paginas">
-                            <h2>Páginas</h2>
-                            <ul>
-                                <li>Login</li>
-                                <li>Home</li>
-                                <li>Listar Serviços</li>
-                                <li>Cadastrar Cliente</li>
-                                <li>Cadastrar Desenvolvedor</li>
-                            </ul>
-                        </div>
-                        <img src={imgLogo} alt="" />
-                        <div className="rodape_conteudo_contatos">
-                            <h2>Contatos</h2>
-                            <div>
-                                <a href="#"><img src={iconFace} alt="" /></a>
-                                <a href="#"><img src={iconInsta} alt="" /></a>
-                                <a href="#"><img src={iconLinkedin} alt="" /></a>
-                            </div>
-                            <a href="mailto:">contato@vsconnect.com</a>
-                        </div>
-                    </div>
-                    <p>todos os direitos reservados ©.</p>
-                </div>
-            </footer>
         </div>
     );
 }
