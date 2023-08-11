5a3e0c384e9180f1252de083ec99f61d2282bcd7
Aléxia <aleeevitoria@gmail.com>
2023-08-11 19:06:05 -0300

    mudança de tag a por Link

--- vsconnect/src/pages/Home/index.tsx
+++ vsconnect/src/pages/Home/index.tsx
@@ -2,62 +2,17 @@
 import "./style.css";
 
 //imagens
-import imgLogo from "../../assets/images/logo.svg";
 import imgClientes from "../../assets/images/clientes.png";
 import imgMaosDev from "../../assets/images/maos_dev.png";
 import imgAlfinete from "../../assets/images/alfinete.png";
 
+//rotas
+import { Link } from "react-router-dom";
 
 function Home() {
 
-    // Mostrar menu no layout responsivo
-    function mostrarMenu() {
-        let sombra = document.getElementById("sombra") as HTMLCanvasElement; // Obtém o elemento da sombra pelo ID
-        let menu: any = document.getElementById("menu_links"); // Obtém o elemento do menu pelo ID
-        let menu_barras: any = document.getElementById("menu_barras"); // Obtém o elemento do botão do menu pelo ID
-
-        let body: any = document.getElementsByTagName("body")[0]; // Obtém o elemento body (primeiro elemento)
-
-        if (window.getComputedStyle(menu).left != "10px") { // Verifica se a propriedade 'left' do estilo computado do elemento do menu é diferente de 10px
-            menu.style.left = "10px"; // Define a posição 'left' do menu como 10px movendo ele para direita
-            sombra.style.right = "-10vw"; // Move a sombra para a direita
-            menu_barras.setAttribute("aria-expanded", "true"); // Atualiza o atributo 'aria-expanded' para 'true'
-            menu_barras.setAttribute("aria-label", "fechar menu"); // Atualiza o atributo 'aria-label' para 'fechar menu'
-            body.style.overflow = "hidden"; // Define o overflow do body como "hidden" para evitar a rolagem da página
-        } else {
-            menu.style.left = "-300px"; // Esconde o menu deslocando-o para esquerda
-            sombra.style.right = "110vw"; // Move a sombra para a esquerda para ocultá-la
-            menu_barras.setAttribute("aria-expanded", "false"); // Atualiza o atributo 'aria-expanded' para 'false'
-            menu_barras.setAttribute("aria-label", "abrir menu"); // Atualiza o atributo 'aria-label' para 'abrir menu'
-            body.style.overflow = "auto"; // Restaura o overflow do body para o valor padrão (auto)
-        }
-        menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
-    }
-
     return (
-        <div>
-            <div id="sombra"></div>
-            <header>
-                <div className="container header_conteudo">
-                    <a className="menu_barras" id="menu_barras" aria-label="abrir menu" aria-expanded="false" aria-controls="menu_links" aria-haspopup="true" onClick={mostrarMenu} href="#"><div className="barras"></div></a>
-                    <img className="header_logo" src={imgLogo} alt="" />
-                    <nav>
-                        <div id="menu_links" className="menu_links">
-                            <a href="../Home/index.html">home</a>
-                            <a href="#">serviços</a>
-                            <a href="../CadastroDev/index.html">cadastro</a>
-                        </div>
-                        <a className="header_icone_login" href="../Login/index.html">
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 512 512">
-                                <path
-                                    d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
-                            </svg>
-                        </a>
-                        <a className="header_botao_login" href="../Login/index.html">login</a>
-                    </nav>
-                </div>
-            </header>
+        <>
             <main id="main_home">
                 <h1>página inicial VSconnect</h1>
                 <section className="banner">
@@ -67,8 +22,8 @@ function Home() {
                             oportunidades.</p>
                         <p className="banner_slogan_l3">Conecte-se e comece a desvendar esse mundo!</p>
                         <div className="banner_botoes">
-                            <a className="botao banner_botao_dev" href="#">desenvolvedor</a>
-                            <a className="botao banner_botao_cli" href="#">cliente</a>
+                            <Link className="botao banner_botao_dev" to="#">desenvolvedor</Link>
+                            <Link className="botao banner_botao_cli" to="#">cliente</Link>
                         </div>
                     </div>
                 </section>
@@ -81,11 +36,11 @@ function Home() {
                         <div className="clientes_texto">
                             <h2>para clientes</h2>
                             <ul>
-                                <li>cadastrar serviços</li>
-                                <li>procurar por desenvolvedores</li>
+                                <li><Link to={""}>cadastrar serviços</Link></li>
+                                <li><Link to={""}>procurar por desenvolvedores</Link></li>
                             </ul>
                             <div>
-                                <a className="botao clientes_botao_cli" href="#">criar conta</a>
+                                <Link className="botao clientes_botao_cli" to="#">criar conta</Link>
                             </div>
                         </div>
                     </div>
@@ -95,11 +50,13 @@ function Home() {
                     <div className="devs_texto">
                         <h2>para desenvolvedores</h2>
                         <ul>
-                            <li>encontrar serviços</li>
-                            <li>divulgar suas hardskills</li>
+                            <li>
+                                <Link to={"#"}>encontrar serviços</Link>
+                            </li>
+                            <li><Link to={"#"}>divulgar suas hardskills</Link></li>
                         </ul>
                         <div>
-                            <a className="botao clientes_botao_devs" href="#">criar conta</a>
+                            <Link className="botao clientes_botao_devs" to="#">criar conta</Link>
                         </div>
                     </div>
                     <img src={imgMaosDev} alt="" />
@@ -122,10 +79,10 @@ function Home() {
                             <p>Proposta: R$750,00</p>
                         </div>
                     </div>
-                    <a href="#">Ver mais serviços</a>
+                    <Link to="#">Ver mais serviços</Link>
                 </section>
             </main>
-        </div>
+        </>
     );
 }
 
