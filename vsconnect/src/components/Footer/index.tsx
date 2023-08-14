44338901cfce75bf6be3a427c4d5c3d63a3e2571
Aléxia <aleeevitoria@gmail.com>
2023-08-14 18:33:59 -0300

    inclusão de condição que verifica rota atual

--- vsconnect/src/components/Footer/index.tsx
+++ vsconnect/src/components/Footer/index.tsx
@@ -9,39 +9,45 @@ import iconLinkedin from "../../assets/images/linkedin.svg";
 import "./style.css";
 
 //rotas
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Footer() {
+
+    const location = useLocation();
+
+
     return (
         //codigo do footer
-        <footer>
+        <footer className={location.pathname == "/" ? "footer_background_image" : "footer_background_color"}>
             <div className="container rodape">
-                <div className="span_dicas">
-                    <img src={imgDev} alt="" />
-                    <div className="span_dicas_texto">
-                        <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
-                            recomendações para saber mais.</p>
-                        <a className="botao botao_dicas" href="#">mais dicas</a>
-                    </div>
-                </div>
+                {
+                    location.pathname == "/" ? <div className="span_dicas">
+                        <img src={imgDev} alt="" />
+                        <div className="span_dicas_texto">
+                            <p>Temos algumas dicas para o seu serviço ou freela ser um sucesso, acesse nossa página de
+                                recomendações para saber mais.</p>
+                            <Link className="botao botao_dicas" to={"#"}>mais dicas</Link>
+                        </div>
+                    </div> : ""
+                }
                 <div className="rodape_conteudo">
                     <div className="rodape_conteudo_paginas">
                         <h2>Páginas</h2>
                         <ul>
                             <li>
-                                <Link to={""}>Login</Link>
+                                <Link to={"#"}>Login</Link>
                             </li>
                             <li>
-                                <Link to={""}>Home</Link>
+                                <Link to={"/"}>Home</Link>
                             </li>
                             <li>
-                                <Link to={""}>Listar Serviços</Link>
+                                <Link to={"#"}>Listar Serviços</Link>
                             </li>
                             <li>
-                                <Link to={""}>Cadastrar Cliente</Link>
+                                <Link to={"#"}>Cadastrar Cliente</Link>
                             </li>
                             <li>
-                                <Link to={""}>Cadastrar Desenvolvedor</Link>
+                                <Link to={"#"}>Cadastrar Desenvolvedor</Link>
                             </li>
                         </ul>
                     </div>
@@ -58,7 +64,7 @@ function Footer() {
                             </Link>
 
                         </div>
-                        <Link to={"mailto:"}>contato@vsconnect.com</Link>
+                        <Link to={"mailto:contato@vsconnect.com"}>contato@vsconnect.com</Link>
                     </div>
                 </div>
                 <p>todos os direitos reservados ©.</p>
