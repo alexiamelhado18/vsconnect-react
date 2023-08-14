33c98587672ffc4219313c4edcb51d20bddf1421
Aléxia <aleeevitoria@gmail.com>
2023-08-14 18:06:27 -0300

    state devs

--- vsconnect/src/pages/ListaDevs/index.tsx
+++ vsconnect/src/pages/ListaDevs/index.tsx
@@ -1,7 +1,41 @@
 //estilização
 import "./style.css";
 
+//components
+import CardDev from "../../components/CardDev";
+
+//hooks
+import { useState } from "react";
+
 function ListaDevs() {
+
+    const [devs, setDevs] = useState<any[]>([
+        {
+            img_perfil: "https://github.com/Thiago-Nascimento.png",
+            nome: "Thiago Nascimento",
+            email: "thiago@email.com",
+            skills: ["HTML", "CSS", "React"]
+        },
+        {
+            img_perfil: "https://github.com/JessicaSanto.png",
+            nome: "Jessica Franzon",
+            email: "jessica@email.com",
+            skills: ["HTML", "CSS", "React"]
+        },
+        {
+            img_perfil: "https://github.com/odirlei-assis.png",
+            nome: "Odirlei Sabella",
+            email: "odirlei@email.com",
+            skills: ["HTML", "CSS", "React"]
+        },
+        {
+            img_perfil: "https://github.com/alexiamelhado18.png",
+            nome: "Aléxia Vitória",
+            email: "alexia@email.com",
+            skills: ["PYTHON", "VUE", "React"]
+        }
+    ]);
+
     return (
         <>
             <main id="main_listadevs">
@@ -25,70 +59,7 @@ function ListaDevs() {
                         </form>
                         <div className="wrapper_lista">
                             <ul>
-                                <li>
-                                    <div className="dev">
-                                        <div className="grupo_contato">
-                                            <img src="https://github.com/Thiago-Nascimento.png" alt="" />
-                                            <div className="contato_dev">
-                                                <h3>Thiago Nascimento</h3>
-                                                <p>thiago@email.com</p>
-                                            </div>
-                                        </div>
-                                        <div className="techs">
-                                            <span>HTML</span>
-                                            <span>CSS</span>
-                                            <span>React</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="dev">
-                                        <div className="grupo_contato">
-                                            <img src="https://github.com/JessicaSanto.png" alt="" />
-                                            <div className="contato_dev">
-                                                <h3>Jessica Franzon</h3>
-                                                <p>jessica@email.com</p>
-                                            </div>
-                                        </div>
-                                        <div className="techs">
-                                            <span>HTML</span>
-                                            <span>CSS</span>
-                                            <span>React</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="dev">
-                                        <div className="grupo_contato">
-                                            <img src="https://github.com/odirlei-assis.png" alt="" />
-                                            <div className="contato_dev">
-                                                <h3>Odirlei Sabella</h3>
-                                                <p>odirlei@email.com</p>
-                                            </div>
-                                        </div>
-                                        <div className="techs">
-                                            <span>HTML</span>
-                                            <span>CSS</span>
-                                            <span>React</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="dev">
-                                        <div className="grupo_contato">
-                                            <img src="https://github.com/alexiamelhado18.png" alt="" />
-                                            <div className="contato_dev">
-                                                <h3>Aléxia Vitória</h3>
-                                                <p>alexia@email.com</p>
-                                            </div>
-                                        </div>
-                                        <div className="techs">
-                                            <span>HTML</span>
-                                            <span>CSS</span>
-                                            <span>React</span>
-                                        </div>
-                                    </div>
-                                </li>
+
                             </ul>
                         </div>
                     </div>
