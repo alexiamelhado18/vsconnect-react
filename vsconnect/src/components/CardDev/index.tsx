cdc1ec6f460c05decf09d4ff3f6c62d6944dbd61
Aléxia <aleeevitoria@gmail.com>
2023-08-14 18:05:58 -0300

    Criação do componente CardDev

--- vsconnect/src/components/CardDev/index.tsx
+++ vsconnect/src/components/CardDev/index.tsx
@@ -1,9 +1,29 @@
 //estilização
 import "./style.css";
 
-function CardDev() {
-    return(
-        <></>
+function CardDev(props: any) {
+    return (
+        <div className="dev">
+            <div className="grupo_contato">
+                <img src={props.foto} alt="" />
+                <div className="contato_dev">
+                    <h3>{props.nome}</h3>
+                    <p>{props.email}</p>
+                </div>
+            </div>
+            <div className="techs">
+
+                {
+                    props.listaTechs.map((tech: string, indice: number) => {
+                        return <span key={indice}>{tech}</span>
+                    })
+                }
+
+                {/* <span>HTML</span>
+                <span>CSS</span>
+                <span>React</span> */}
+            </div>
+        </div>
     );
 }
 
