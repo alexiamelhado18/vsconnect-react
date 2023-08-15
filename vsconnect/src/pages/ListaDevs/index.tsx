df7f09f53e0f2a2c5e921edb66b160eac82222f7
Aléxia <aleeevitoria@gmail.com>
2023-08-14 21:49:54 -0300

    inclusão de funções que filtra por desenvolvedor pela skill digitada

--- vsconnect/src/pages/ListaDevs/index.tsx
+++ vsconnect/src/pages/ListaDevs/index.tsx
@@ -9,12 +9,13 @@ import { useState } from "react";
 
 function ListaDevs() {
 
+    //STATE DEVS
     const [devs, setDevs] = useState<any[]>([
         {
             img_perfil: "https://github.com/Thiago-Nascimento.png",
             nome: "Thiago Nascimento",
             email: "thiago@email.com",
-            skills: ["HTML", "CSS", "React"]
+            skills: ["HTML", "CSS", "REACT"]
         },
         {
             img_perfil: "https://github.com/JessicaSanto.png",
@@ -36,6 +37,35 @@ function ListaDevs() {
         }
     ]);
 
+    const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs);
+
+    const [skillDigitado, setSkillDigitado] = useState<string>("");
+
+    //função onde pega o que o usuario digitou
+    function verificarCampoSkill(event: any) {
+        if (event.target.value === "") {
+            setListaDevsFiltrados(devs);
+        }
+        setSkillDigitado(event.target.value);
+    }
+
+    function buscarDevPorSkill(event: any) {
+        //não recarrega a pagina
+        event.preventDefault();
+
+        //filtrar devs pela skill digitada no campo buscar
+        const devsFiltrados = devs.filter((dev: any) => dev.skills.includes(skillDigitado.toLocaleUpperCase()));
+
+        if (devsFiltrados.length === 0) {
+            alert("Nenhum desenvolvedor(a) com essa skill :(")
+        } else {
+            //atribui valor de devs filtrado, ao state ListaDevsFiltrados 
+            setListaDevsFiltrados(devsFiltrados);
+        }
+
+
+    }
+
     return (
         <>
             <main id="main_listadevs">
@@ -43,7 +73,7 @@ function ListaDevs() {
                     <div className="lista_devs_conteudo">
                         <h1>Lista de Devs</h1>
                         <hr />
-                        <form method="post">
+                        <form method="post" onSubmit={buscarDevPorSkill}>
                             <div className="wrapper_form">
                                 <label htmlFor="busca">Procurar desenvolvedores</label>
                                 <div className="campo-label">
@@ -52,6 +82,8 @@ function ListaDevs() {
                                         name="campo-busca"
                                         id="busca"
                                         placeholder="Buscar desenvolvedores por tecnologias..."
+                                        autoComplete="off"
+                                        onChange={verificarCampoSkill}
                                     />
                                     <button type="submit">Buscar</button>
                                 </div>
@@ -59,7 +91,18 @@ function ListaDevs() {
                         </form>
                         <div className="wrapper_lista">
                             <ul>
-
+                                {
+                                    listaDevsFiltrados.map((dev: any, indice: number) => {
+                                        return <li key={indice}>
+                                            <CardDev
+                                                foto={dev.img_perfil}
+                                                nome={dev.nome}
+                                                email={dev.email}
+                                                listaTechs={dev.skills}
+                                            />
+                                        </li>
+                                    })
+                                }
                             </ul>
                         </div>
                     </div>
