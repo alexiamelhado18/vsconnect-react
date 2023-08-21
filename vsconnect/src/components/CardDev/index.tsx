273bf531e915cf39b2d62b717930d1fc49fa582a
Aléxia <aleeevitoria@gmail.com>
2023-08-21 18:57:08 -0300

    adicionado link para redirecionar ao perfil do respectivo dev

--- vsconnect/src/components/CardDev/index.tsx
+++ vsconnect/src/components/CardDev/index.tsx
@@ -1,13 +1,16 @@
 //estilização
 import "./style.css";
 
+//rotas
+import { Link } from "react-router-dom";
+
 function CardDev(props: any) {
     return (
         <div className="dev">
             <div className="grupo_contato">
                 <img src={"http://localhost:3000/static/" + props.foto} alt="" />
                 <div className="contato_dev">
-                    <h3>{props.nome}</h3>
+                    <Link to={"/perfil/" + props.id}><h3>{props.nome}</h3></Link>
                     <p>{props.email}</p>
                 </div>
             </div>
