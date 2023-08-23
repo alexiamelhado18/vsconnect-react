2a4b560baf7826e2dde5669764e5ee19c34fdb24
Aléxia <aleeevitoria@gmail.com>
2023-08-23 20:35:18 -0300

    rota do componente CadastroServico criada

--- vsconnect/src/main.tsx
+++ vsconnect/src/main.tsx
@@ -8,6 +8,7 @@ import ListaDevs from "./pages/ListaDevs";
 import Perfil from "./pages/PerfilUsuario";
 import VisualizarServico from "./pages/VisualizarServico";
 import CadastroUsuario from './pages/CadastroUsuario';
+import CadastroServico from './pages/CadastroServico';
 import Footer from "./components/Footer"
 import Header from './components/Header';
 
@@ -31,6 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='perfil/:idUsuario' element={<Perfil />} />
         <Route path='visualizar/servico/:idServico' element={<VisualizarServico />} />
         <Route path='cadastro/usuario' element={<CadastroUsuario />} />
+        <Route path='cadastro/servico' element={<CadastroServico />} />
       </Routes>
       <Footer />
     </BrowserRouter>
