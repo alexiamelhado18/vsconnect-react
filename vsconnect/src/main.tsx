f80f2dd65ce214c896fa087c0bed1bb93c44eb27
Aléxia <aleeevitoria@gmail.com>
2023-08-25 18:26:51 -0300

    função que verifica se o usuario esta logado

--- vsconnect/src/main.tsx
+++ vsconnect/src/main.tsx
@@ -9,6 +9,7 @@ import Perfil from "./pages/PerfilUsuario";
 import VisualizarServico from "./pages/VisualizarServico";
 import CadastroUsuario from './pages/CadastroUsuario';
 import CadastroServico from './pages/CadastroServico';
+import Login from './pages/Login';
 import Footer from "./components/Footer"
 import Header from './components/Header';
 
@@ -18,10 +19,27 @@ import "./index.css";
 //rotas
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//
+import secureLocalStorage from 'react-secure-storage';
+
+function logado() {
+  if (secureLocalStorage.getItem("user")) {
+    const objetoUsuario: any = secureLocalStorage.getItem("user");
+
+    const nome: string = objetoUsuario.user.nome.trim().split(" ")[0];
+
+    return { logado: true, nomeUsuario: nome }
+  }
+  else {
+    return { logado: false, nomeUsuario: null }
+  }
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter> {/*Indica que aplicação terá rotas*/}
-      <Header />
+      <Header usuario={logado()} />
       <Routes>{/*Indica uma lista de rotas*/}
         <Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
         <Route path='lista/servicos' element={<ListaServicos />} />
@@ -33,6 +51,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='visualizar/servico/:idServico' element={<VisualizarServico />} />
         <Route path='cadastro/usuario' element={<CadastroUsuario />} />
         <Route path='cadastro/servico' element={<CadastroServico />} />
+        <Route path='login' element={<Login />} />
       </Routes>
       <Footer />
     </BrowserRouter>
