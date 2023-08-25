abfb4405de7f8d8f673893c2479c450e579d3154
Aléxia <aleeevitoria@gmail.com>
2023-08-25 18:27:35 -0300

    Adicionado props ao componente Header e condição que verifica se o usuario esta logado

--- vsconnect/src/components/Header/index.tsx
+++ vsconnect/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import imgLogo from "../../assets/images/logo.svg";
 //rotas
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header(props: any) {
 
     // Mostrar menu no layout responsivo
     function mostrarMenu() {
@@ -53,7 +53,23 @@ function Header() {
                                     d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
                             </svg>
                         </Link>
-                        <Link className="header_botao_login" to="/login">login</Link>
+
+                        {
+                            props.usuario.logado ?
+                                <span>Olá, {props.usuario.nomeUsuario}</span>
+                                :
+                                <Link className="header_botao_login" to="/login">login</Link>
+                        }
+
+                        {/* 
+                            if (props.usuario.logado) {
+                                return <span>Olá, {props.usuario.nomeUsuario}</span> 
+                            }else{
+                                return <Link className="header_botao_login" to="/login">login</Link>
+                            }
+                         */}
+
+
                     </nav>
                 </div>
             </header>
